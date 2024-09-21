4cf119616a5fd9f6be8e1d9531a6d6aa732bb749
Ryan McKay <ryan.michael.mckay@gmail.com>
2024-09-21 18:19:54 -0500

    add some AUTHOR config

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,6 +14,8 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+AUTHOR_GITHUB = "ryanmckaytx"
+AUTHOR_LINKEDIN = "ryanmckaytx"
 THEME = "./pelican-mediumfox"
 
 # Blogroll
